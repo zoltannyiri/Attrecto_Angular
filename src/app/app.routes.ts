1ff7a20da350a1e2524d109e8e6077bc9b4c61e3
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-05-23 16:34:50 +0200

    angular_project_upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,8 @@ export const routes: Routes = [
 {'path': '', redirectTo: '/home', pathMatch: 'full' },
 {'path': 'home', component: HomeComponent },
 {'path':  'movies', component:MoviesComponent},
-{'path': 'sandbox', component:SandboxComponent}
+{'path': 'sandbox', component:SandboxComponent},
+{'path': '**', redirectTo: '/sandbox', pathMatch: 'full'}
 
 ];
 
